4bc6af43d2d7affd5db68e6bb171d8e00b1cc56d
Christoph Beger <cbeger@imise.uni-leipzig.de>
2017-04-13 09:44:05 +0200

    added CR and LF to test

--- t/lib/Record.pm
+++ t/lib/Record.pm
@@ -5,7 +5,7 @@ use xDT::Record;
 sub test_constructor {
     my $test = shift;
 
-    isa_ok my $record = xDT::Record->new('01380006311'), 'xDT::Record';
+    isa_ok my $record = xDT::Record->new("01380006311\r\n"), 'xDT::Record';
     is $record->getId, 8000, '... id should be 8000';
     is $record->getLength, '013', '... length should be 013';
     is $record->getValue, '6311', '... value should be "6311"';
